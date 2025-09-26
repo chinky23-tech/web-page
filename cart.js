be3ebd89bed35685f16776cb9d737a88730ac681
chinky23-tech <chinky0330@gmail.com>
2025-09-26 23:11:57 +0530

    foreach method

---
+++ cart.js
@@ -0,0 +1,14 @@
+const fruits = ['apple' , 'grapes' , 'mango' , 'orange'];
+
+
+//select the output div
+
+const outputDiv = document.getElementById('output');
+
+//use forEach to loop through items
+fruits.forEach(function(fruit, index){
+
+const ol = document.createElement('li');
+ol.textContent = `${index + 1}. ${fruit}`;
+outputDiv.appendChild(ol);
+});
