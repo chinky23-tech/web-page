94ff8d799b4f36b47226f101a20b85b68521e045
chinky23-tech <chinky0330@gmail.com>
2025-09-30 23:23:51 +0530

    cart

--- cart.js
+++ cart.js
@@ -31,3 +31,57 @@ function renderProducts(productList){
     
   });
 }
+
+//step 3: event delegation for Add to Cart
+products.addEventListener('click', e =>{
+
+if(e.target.classList.contains('addBtn')){
+  const id = Number(e.target.dataset.id);
+  addToCart(id);
+}
+
+});
+
+//step 4: event delegation for remove from cart
+
+cartItems.addEventListener('click' , e => {
+if(e.target.classList.contains('removeBtn')){
+  const index = Number(e.target.dataset.index);
+  removeFromCart(index);
+}
+});
+// Cart Functions
+async function addToCart(id) {
+  // fetch single product details (from API)
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const item = await res.json();
+
+  cart.push(item);
+  displayCart();
+}
+
+function removeFromCart(index) {
+  cart = cart.filter((_, i) => i !== index);
+  displayCart();
+}
+
+function displayCart() {
+  cartItems.innerHTML = '';
+
+  cart.forEach((item, index) => {
+    const div = document.createElement('div');
+    div.className = "flex justify-between items-center border-b py-1";
+    div.innerHTML = `
+      <span>${item.title} - ₹${Math.floor(item.price * 80)}</span>
+      <button class="removeBtn text-red-500 hover:text-red-700" data-index="${index}">
+        Remove
+      </button>`;
+    cartItems.appendChild(div);
+  });
+
+  const total = cart.reduce((sum, item) => sum + item.price * 80, 0);
+  cartSummary.textContent = `Total Items: ${cart.length} | Total Price: ₹${total}`;
+}
+
+// Load products on page start
+loadProducts();
